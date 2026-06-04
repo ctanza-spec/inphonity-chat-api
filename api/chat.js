56d00d8f8c11213515fae074872139763ea0bdb0
ctanza-spec <ctanza@inphonity.com>
2026-06-04 10:58:17 -0600

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,12 +1,10 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  // CORS - Permitir desde cualquier origen
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
+  // Manejar preflight
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
